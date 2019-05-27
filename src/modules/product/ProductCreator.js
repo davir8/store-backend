9d51bb3ac56ed9841ff1b202e36fc7d639c5a916
Davi <davirodrigues8@gmail.com>
2019-05-27 10:38:16 -0300

    corrects field userId in model user

--- src/modules/product/ProductCreator.js
+++ src/modules/product/ProductCreator.js
@@ -1,9 +1,19 @@
 import Product from "./ProductModel";
 
-export const createProduct = async (root, args, context) => {
+export const createProduct = async (root, args, { user }) => {
+  // make sure user is logged in
+  if (!user) {
+    throw new Error("You are not authenticated!");
+  }
+
   const { name, description, url, userId } = args;
 
-  const product = await Product.create({ name, description, url, owner: userId })
-  
+  const product = await Product.create({
+    name,
+    description,
+    url,
+    owner: userId
+  });
+
   return await product.populate("owner").execPopulate();
 };
